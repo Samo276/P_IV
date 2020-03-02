f24cd42018fed6ff725d44a8d66dafa5f3f5ee05
Samo276 <marek276@gmail.com>
2020-03-02 16:47:42 +0100

    P4-lab1
    @Samo276

---
+++ 01.lab1netcore/lab1netcore/lab1netcore/DB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+namespace lab1netcore
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+            var sql = "SELECT * FROM region";
+            var command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0]+" "+ reader[1]);
+            }
+            reader.Close();
+        }
+        public void Insert(SqlConnection connection, int id, string regionName)
+        {
+            var sql = "INSERT INTO Region(RegionId, RegionDescription) VALUES (@id, '@regionName')";
+            var command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@id", id);
+            command.Parameters.AddWithValue("@regionName", regionName);
+
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+        public void Delete(SqlConnection connection, int id)
+        {
+            var sql = "DELETE FROM Region(RegionId, RegionDescription) WHERE RegionId = @id";
+            var command = new SqlCommand(sql, connection);
+            //command.Parameters.AddWithValue("@id", id);
+            //command.Parameters.AddWithValue("@regionName", regionName);
+
+
+            int affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows inserted");
+        }
+    }
+}
---
+++ 01.lab1netcore/lab1netcore/lab1netcore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace lab1netcore
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionStrig = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //t wrzucamy adres
+            using var connection = new SqlConnection(connectionStrig);
+
+            var db = new DB();
+            connection.Open();
+
+
+            db.Select(connection);
+            //dopisać insert i delete
+            //db.Insert(connection, 276, "Bielsko");
+            db.Delete(connection, 276);
+            connection.Close();
+        }
+
+
+    }
+}
